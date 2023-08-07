4d991d1214e31d2f1c43baf1cf5ba6fd5432bdfd
경제목 <jemok9605@naver.com>
2023-08-08 02:04:06 +0900

    section4[엔티티 매핑]까지 새롭게 정리

--- Jpa_ex1/src/main/java/hellojpa/JpaMain.java
+++ Jpa_ex1/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.transaction.TransactionManager;
 import java.util.List;
 
 public class JpaMain {
@@ -12,44 +13,19 @@ public class JpaMain {
         EntityManager em = emf.createEntityManager();
 
         EntityTransaction tx = em.getTransaction();
+
         tx.begin();
         try{
-            //팀 저장
-            Team team = new Team();
-            team.setName("TeamA");
-            em.persist(team);
-            //회원 저장
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setTeam(team); //연관관계 설정, 참조 저장
+            Member member = new Member(200L, "member200");
             em.persist(member);
-
-
-//            em.flush();
-//            em.clear();
-            //조회
-            Member findMember = em.find(Member.class, member.getId());
-            Team findTeam = findMember.getTeam();
-
-            Team team1 = em.find(Team.class, team.getId());
-            List<Member> members1= team1.getMembers();
-            System.out.println("============================");
-            for (Member m: members1) {
-                System.out.println(m.getUsername());
-            }
-            System.out.println("============================");
-
-
-            List<Member> members = findTeam.getMembers();
-            for (Member m: members) {
-                System.out.println("m = " + m.getUsername());
-            }
-
-
+            em.flush();
+            System.out.println(" ================ ");
             tx.commit();
-        }catch (Exception e) {
+
+        }
+        catch(Exception e) {
             tx.rollback();
-        }finally{
+        }finally {
             em.close();
         }
         emf.close();
--- Jpa_ex1/src/main/java/hellojpa/Member.java
+++ Jpa_ex1/src/main/java/hellojpa/Member.java
@@ -1,65 +1,28 @@
 package hellojpa;
 
-
-
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
-import javax.persistence.*;
-import java.time.LocalDateTime;
-import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
-
+@Entity
 @Getter
 @Setter
-@Entity
-//@Table //엔티티와 매핑할 테이블 지정
+@ToString
 public class Member {
 
-    @Id  //PK매핑
-    @GeneratedValue//AUTO_INCREMENT
-    @Column(name = "MEMBER_ID")
+    @Id
     private Long id;
-    
-    @Column(name="USERNAME") // DB의 컬럼명, 자바에서의 변수명 다를경우  Column(name)으로 DB컬럼 명 지정 가능함
-    private String username;
+    private String name;
 
-    @ManyToOne //Member입장에서 N  TEAM입장에서 1
-    @JoinColumn(name = "TEAM_ID") //어떤 값으로 조인할 것인지
-    private Team team;
-
-    public void setTeam(Team team) {
-        this.team = team;
-        team.getMembers().add(this);  // 순수 객체 상태를 고려해서 항상 양쪽에 값을 설정하자
-    }
-
-    //외래키 값을 테이블에 맞춰 그대로 가져옴 객체지향적이지 않음
-//    @Column(name = "TEAM_ID")
-//    private Long teamId;
-
-
-
-
-
-
-
-//    @Enumerated(EnumType.STRING)//EnumType.STRING 써야함 EnumType.ORDINAL은 enum추가 시 순서에 의한 문제가 생길 수 있음
-//    private RoleType roleType;
-//
-//    @Temporal(TemporalType.TIMESTAMP)
-//    private Date createdDate;
-//
-//    @Temporal(TemporalType.TIMESTAMP) //날짜 타입 매핑
-//    private Date lastModifiedDate;
-//
-//    //@Temporal(TemporalType.TIMESTAMP)이거 대신 그냥 LocalDateTiem(년월일 시분초)이나 LocalDate(년월일)쓰면됨
-////    private LocalDateTime testLocal;
-//    @Lob // BLOB, CLOB 매핑
-//    private String description;
 
     public Member() {
     }
 
-
-
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
