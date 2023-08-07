131d21c7931d923f40d098d55d6a8b9083fe45d8
경제목 <jemok9605@naver.com>
2023-08-08 04:24:10 +0900

    매핑 연습

--- Jpa_ex1/src/main/java/hellojpa/Member.java
+++ Jpa_ex1/src/main/java/hellojpa/Member.java
@@ -5,6 +5,8 @@ import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
@@ -13,7 +15,7 @@ import javax.persistence.Id;
 @ToString
 public class Member {
 
-    @Id
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
