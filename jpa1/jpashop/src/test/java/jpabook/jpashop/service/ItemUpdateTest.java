22c731ae9db5dc9c067a9462a5b38b90e0f58d6f
경제목 <jemok9605@naver.com>
2023-08-29 04:40:15 +0900

    JAP활용1 완료

---
+++ jpa1/jpashop/src/test/java/jpabook/jpashop/service/ItemUpdateTest.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.item.Book;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.persistence.EntityManager;
+import javax.swing.*;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ItemUpdateTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    public void updateTest() throws Exception{
+        Book book = em.find(Book.class, 1L);
+
+        book.setName("asda"); // 영속상태일 경우에 변경감지를 통해서 자동으로 DB에 업데이트를함
+
+    }
+}
