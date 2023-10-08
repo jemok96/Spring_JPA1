ad9654dc8e09011177fcc0e976f8fe141632b961
경제목 <jemok9605@naver.com>
2023-10-09 01:09:11 +0900

    JPA2 회원 수정 api

--- jpa2/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ jpa2/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -2,9 +2,7 @@ package jpabook.jpashop.api;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -26,6 +24,7 @@ public class MemberApiController {
     }
     /***
      * 회원등록 V2
+     * Entity로 받지 말고 DTO를 만들어서 받자
      */
     @PostMapping("/api/v2/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid CreateMemberRequest request) {
@@ -34,6 +33,10 @@ public class MemberApiController {
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
     }
+
+    /***
+     * 회원 수정 api
+     */
     @PatchMapping("/api/v2/members/{id}")
     public UpdateMemberResponse updateMemberV2(
             @PathVariable("id")Long id,
@@ -58,7 +61,9 @@ public class MemberApiController {
         private String name;
     }
 
-    @Data
+    //dto임 이게 
+    @RequiredArgsConstructor
+    @Getter
     static class CreateMemberResponse{
         private Long id;
 
