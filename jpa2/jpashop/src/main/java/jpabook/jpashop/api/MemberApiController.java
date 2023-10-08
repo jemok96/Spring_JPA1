211aaae154eeb2235ff32f62542f6cfc345a171a
경제목 <jemok9605@naver.com>
2023-10-09 00:28:28 +0900

    JPA 정리

--- jpa2/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ jpa2/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -2,11 +2,10 @@ package jpabook.jpashop.api;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -15,11 +14,48 @@ import javax.validation.Valid;
 public class MemberApiController {
     private final MemberService memberService;
 
+    /***
+     * 회원등록 V1
+     */
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member){
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
+        //Entity자체를 바로 사용하는 것은 안좋음 Entity에 validation도 사용하면 너무 복잡해지고 책임이 많아짐
+        //Member로 받으면 어떤 값이 바인딩돼서 넘어 오는지도 모름
+    }
+    /***
+     * 회원등록 V2
+     */
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid CreateMemberRequest request) {
+        Member member = new Member();
+        member.setName(request.getName());
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+    @PatchMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(
+            @PathVariable("id")Long id,
+            @RequestBody @Valid UpdateMemberRequest request){
+        memberService.update(id,request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
 
+    @Data
+    static class UpdateMemberRequest{
+        private String name;
+    }
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse{
+        private Long id;
+        private String name;
+    }
+    @Data
+    static class CreateMemberRequest{
+        private String name;
     }
 
     @Data
