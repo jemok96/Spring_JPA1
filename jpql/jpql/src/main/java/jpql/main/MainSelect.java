e755d2a6950ccdf413b320b7d75695316076a605
경제목 <jemok9605@naver.com>
2023-10-09 00:28:39 +0900

    jpql 정리

--- jpql/jpql/src/main/java/jpql/main/MainSelect.java
+++ jpql/jpql/src/main/java/jpql/main/MainSelect.java
@@ -23,22 +23,24 @@ public class MainSelect {
             member.setTeam(team);
             em.persist(member);
 
-            //프로젝션: SELECT절에 조회할 대상을 지정하는 것
-            //엔티티 프로젝션
-            List<Team> result = em.createQuery("select m.team from Member m", Team.class).getResultList();
-            System.out.println("result = " + result);
-
             Order order = new Order();
             order.setAddress(new Address("city","street","1000"));
             em.persist(order);
+            //프로젝션: SELECT절에 조회할 대상을 지정하는 것
+            //엔티티 프로젝션
+            List<Team> result = em.createQuery("select m.team from Member m", Team.class).
+                    getResultList();
+            System.out.println("result = " + result);
 
             //임베디드 타입 프로젝션
-            List<Address> resultList = em.createQuery("select m.address from Order m").getResultList();
+            List resultList = em.createQuery("select m.address from Order m",Address.class).
+                    getResultList();
             System.out.println("resultList = " + resultList);
 
             //스칼라 타입 프로젝션, 생성자 필요
             List<MemberDTO> list = em.createQuery("select new jpql.MemberDTO(m.username,m.age) from Member m", MemberDTO.class)
                     .getResultList();
+
             MemberDTO memberDTO = list.get(0);
             System.out.println("memberDTO = " + memberDTO.getUsername());
             tx.commit();
